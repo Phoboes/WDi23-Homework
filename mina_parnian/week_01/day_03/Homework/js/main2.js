83dee9db260188fd5ac35f16cd2e97a79771448d
Mina Parnian <minaparnian66@gmail.com>
2017-07-26 18:48:30 +1000

    homework

---
+++ mina_parnian/week_01/day_03/Homework/js/main2.js
@@ -0,0 +1,22 @@
+/*
+3- Write a function that takes a character (i.e. a string of length 1) and returns 
+true if it is a vowel, false otherwise.
+*/
+
+
+let vowelCharacter = function (a) {
+	if (a === "s" || a === "l" || a === "s" || a === "b") {
+		console.log("This is vowel.");
+		
+	} else {
+		console.log("This is not a vowel");
+		
+	}
+
+	
+};
+
+vowelCharacter("s");
+vowelCharacter("b");
+vowelCharacter("c");
+vowelCharacter("t");
