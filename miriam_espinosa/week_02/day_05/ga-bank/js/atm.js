71a2c527d6caa10f60e4d17f9940dd153cf6d6bf
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-08-04 23:39:31 +1000

    ATM change color to red when 0 and does not go negative amount

--- miriam_espinosa/week_02/day_05/ga-bank/js/atm.js
+++ miriam_espinosa/week_02/day_05/ga-bank/js/atm.js
@@ -1,26 +1,73 @@
 //logic code
+
 bank = {
   checking:0,
+  saving:0,
   //equivalent to:
   //deposit: function () {
   deposit(account, amount) {
     this[account]+=parseFloat(amount);
-  }
+  },
+
+  withdraw(account, amount) {
+    this[account]-=parseFloat(amount);
+  },
+
 };
+
 
 //UI/DOM code
 
 const render = function (){
   $('#checking-balance').text('$'+ bank.checking);
-  //all DOM updating code will be here
+  $('#savings-balance').text('$'+ bank.saving);
+  backgroundColor();
 }
 
+
+//all DOM updating code will be here
+
 $(document).ready(function(){
+
   $('#checking-deposit').on('click', function(){
     const amount = $('#checking-amount').val();
     bank.deposit('checking', amount);
+
+    render();
+  });
+
+  $('#checking-withdraw').on('click', function(){
+    const amount = $('#checking-amount').val();
+    if (bank.checking >= amount){
+      bank.withdraw('checking', amount);
+    }else {
+      alert('You don not have enough money to withdraw')
+    }
+    render();
+  });
+
+  $("#savings-deposit").on("click",function(){
+    const amount = $('#savings-amount').val();
+    bank.deposit('saving', amount);
+
     render(); //update the screen
+  });
+
+  $('#savings-withdraw').on('click', function(){
+    const amount = $('#savings-amount').val();
+    if (bank.saving >= amount){
+      bank.withdraw('saving', amount);
+    }else {
+      alert ('You don not have enough money to withdraw')
+    }
+    render();
   });
 });
 
-//cant upload anything that is not money
+let backgroundColor = function(){
+  if (bank.checking === 0){
+    $('#checking-balance').css('background-color', '#F52F4F');
+  } else {
+    $('#checking-balance').css('background-color', '#E3E3E3');
+  }
+}
