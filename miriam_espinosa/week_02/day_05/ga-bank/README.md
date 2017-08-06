7e0457b1c460e4778b6bd662b0410537ca0426ea
Miriam Espinosa Jorrin <miriam@192-168-1-2.tpgi.com.au>
2017-08-06 18:42:09 +1000

    ATM homework finished

--- miriam_espinosa/week_02/day_05/ga-bank/README.md
+++ miriam_espinosa/week_02/day_05/ga-bank/README.md
@@ -24,8 +24,6 @@ so a user can see the change.
 withdraw more money than exists in the account, ignore the transaction.
 * When the balance of the bank account is $0 the background of that bank account
 should be red. It should be gray when there is money in the account.
-
-//to be done
 * What happens when the user wants to withdraw more money from the checking
 account than is in the account? These accounts have overdraft protection, so if
 a withdrawal can be covered by the balances in both accounts, take the checking
--- miriam_espinosa/week_02/day_05/ga-bank/js/atm.js
+++ miriam_espinosa/week_02/day_05/ga-bank/js/atm.js
@@ -7,11 +7,41 @@ bank = {
   //deposit: function () {
   deposit(account, amount) {
     this[account]+=parseFloat(amount);
+
   },
 
   withdraw(account, amount) {
-    this[account]-=parseFloat(amount);
+    if (this[account] >= amount) {
+      this[account]-=parseFloat(amount);
+    }else if ((bank.checking + bank.saving)>= amount){
+      if(account === "checking"){
+        amount -= bank.checking;
+        bank.checking = 0;
+        bank.saving -= parseFloat(amount);
+      }else{
+        amount-= parseFloat(bank.saving);
+        bank.saving = 0;
+        bank.checking -= parseFloat(amount);
+      }
+
+    }else {
+      alert('You don not have enough money to withdraw')
+    }
   },
+
+  backgroundColor(){
+    if (bank.checking === 0){
+      $('#checking-balance').addClass('zero');
+    } else {
+      $('#checking-balance').removeClass('zero');
+    }
+
+    if (bank.saving === 0){
+      $('#savings-balance').addClass('zero');
+    } else {
+      $('#savings-balance').removeClass('zero');
+    }
+  }
 
 };
 
@@ -19,16 +49,18 @@ bank = {
 //UI/DOM code
 
 const render = function (){
-  backgroundColor();
+
   $('#checking-balance').text('$'+ bank.checking);
   $('#savings-balance').text('$'+ bank.saving);
-
+  bank.backgroundColor();
 }
 
 
 //all DOM updating code will be here
 
 $(document).ready(function(){
+
+  bank.backgroundColor();
 
   $('#checking-deposit').on('click', function(){
     const amount = $('#checking-amount').val();
@@ -39,11 +71,8 @@ $(document).ready(function(){
 
   $('#checking-withdraw').on('click', function(){
     const amount = $('#checking-amount').val();
-    if (bank.checking >= amount){
-      bank.withdraw('checking', amount);
-    }else {
-      alert('You don not have enough money to withdraw')
-    }
+    bank.withdraw('checking', amount);
+
     render();
   });
 
@@ -57,25 +86,8 @@ $(document).ready(function(){
 
   $('#savings-withdraw').on('click', function(){
     const amount = $('#savings-amount').val();
-    if (bank.saving >= amount){
-      bank.withdraw('saving', amount);
-    }else {
-      alert ('You don not have enough money to withdraw')
-    }
+    bank.withdraw('saving', amount);
+
     render();
   });
 });
-
-let backgroundColor = function(){
-  if (bank.checking === 0){
-    $('#checking-balance').addClass('zero');
-  } else {
-    $('#checking-balance').removeClass('zero');
-  }
-
-  if (bank.saving === 0){
-    $('#savings-balance').addClass('zero');
-  } else {
-    $('#savings-balance').removeClass('zero');
-  }
-}
