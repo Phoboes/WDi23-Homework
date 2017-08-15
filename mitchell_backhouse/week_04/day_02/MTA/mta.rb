4ad2e9dba61441819fc9387665cd32210406b324
Mitchell Backhouse <mitchell.backhouse@gmail.com>
2017-08-15 23:15:28 +1000

    basic working

--- mitchell_backhouse/week_04/day_02/MTA/mta.rb
+++ mitchell_backhouse/week_04/day_02/MTA/mta.rb
@@ -2,10 +2,6 @@ require 'pry'
 require 'colorize'
 require 'io/console'
 
-# The N line has the following stops: Times Square, 34th, 28th, 23rd, Union Square, and 8th
-# The L line has the following stops: 8th, 6th, Union Square, 3rd, and 1st
-# The 6 line has the following stops: Grand Central, 33rd, 28th, 23rd, Union Square, and Astor Place.
-
 class Line
     attr_reader :stations, :change_point_index
 
@@ -17,16 +13,235 @@ end
 
 class Travel
     # manip methods for travel
+    line_N = Line.new ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'], 'Union Square'
+    line_L = Line.new ['8th', '6th', 'Union Square', '3rd', '1st'], 'Union Square'
+    line_6 = Line.new ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'], 'Union Square'
+
+    @@lines = [line_N, line_L, line_6]
 
     # init counter for trip
-    trip_counter = 0
+    @@trip_counter = 0
+
+    def self.plan_trip sl, ss, el, es # 1, 2, 2, 5
+
+        start_line = @@lines[sl - 1]
+        end_line = @@lines[el - 1]
+        # same line
+        start_index = ss - 1
+        end_index = es - 1
+
+        if start_line == end_line
+            if start_index < end_index
+                p start_line.stations[start_index..end_index]
+                @@trip_counter = start_line.stations[start_index..end_index].size
+                puts "This trip took #{@@trip_counter - 1} stops"
+
+            else
+                p start_line.stations[end_index..start_index].reverse
+                @@trip_counter = start_line.stations[end_index..start_index].size
+                puts "This trip took #{@@trip_counter - 1} stops"
+
+            end
+        else
+            # start line journey to exchange point
+            if start_index < start_line.change_point_index
+                start_leg = start_line.stations[start_index..start_line.change_point_index]
+                end_leg = build_end_leg end_line, end_index
+
+                @@trip_counter =  (start_leg + end_leg).size
+                # return trip
+                puts start_leg.to_s.green
+                puts end_leg.to_s.red
+                puts "This trip took #{@@trip_counter - 1} stops"
+                trip = start_leg, end_leg
+            else
+                start_leg = start_line.stations[start_line.change_point_index..start_index].reverse
+                end_leg = build_end_leg end_line, end_index
+
+                @@trip_counter =  (start_leg + end_leg).size
+                # return trip
+                puts "#{sl}, #{ss} - #{start_leg.to_s}".green
+                puts "#{el}, #{es} - #{end_leg.to_s}".red
+                puts "This trip took #{@@trip_counter - 1} stops"
+
+                trip = start_leg, end_leg
+            end
+        end
+        # line_N.stations[line_N.stations.index('Times Square')..line_N.stations.index('Union Square')]
+    end
+
+    def self.trip_counter
+        @@trip_counter
+    end
+
+    private
+
+    def self.build_end_leg end_line, end_index
+        if end_index < end_line.change_point_index
+            end_leg = end_line.stations[end_index..(end_line.change_point_index - 1)].reverse
+        else
+            end_leg = end_line.stations[(end_line.change_point_index + 1)..end_index]
+        end
+    end
+end
+
+class Render
+    @@start_line_number = 0
+    @@start_station_number = 0
+    @@end_line_number = 0
+    @@end_station_number = 0
+
+    def self.render_menu
+        system 'clear'
+        puts  "┌───────────────────────────────────────────────────────────────────────────┐".white
+        print "│".white
+        print  "                                   Lines                                   ".black.on_light_white
+        puts "│".white
+        puts  "├───┬───────────────────┬───┬──────────────────────┬───┬────────────────────┤".white
+        # puts  "│ 1 │     Line N        │ 2 │       Line L         │ 3 │     Line 6         │".white
+        print "│ 1 │"
+        @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print       "    Line N       "
+        @@end_line_number == 1 ? print(set_color"red") : print(" ")
+        print "│ 2 │"
+        @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print    "      Line L        "
+        @@end_line_number == 2 ? print(set_color"red") : print(" ")
+        print "│ 3 │"
+        @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print  "    Line 6        "
+        @@end_line_number == 3 ? print(set_color"red") : print(" ")
+        puts "│".white
+        puts  "├───┴───────────────────┴───┴──────────────────────┴───┴────────────────────┤".white
+        print  "│".white
+        print "                                  Stations                                 ".black.on_light_white
+        puts  "│".white
+        puts  "├───┬───────────────────┬───┬──────────────────────┬───┬────────────────────┤".white
+        print "│ 1 │"
+        @@start_station_number == 1 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print " Times Square    "
+        @@end_station_number == 1 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│ 1 │"
+        @@start_station_number == 1 && @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print "       8th          "
+        @@end_station_number == 1 && @@end_line_number == 2 ? print(set_color "red") : print(" ")
+        print "│ 1 │"
+        @@start_station_number == 1 && @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print "Grand Central     "
+        @@end_station_number == 1 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print "│\n".white
+        print "│ 2 │"
+        @@start_station_number == 2 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print "     34th        "
+        @@end_station_number == 2 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│ 2 │"
+        @@start_station_number == 2 && @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print "       6th          "
+        @@end_station_number == 2 && @@end_line_number == 2 ? print(set_color "red") : print(" ")
+        print "│ 2 │"
+        @@start_station_number == 2 && @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print "     33rd         "
+        @@end_station_number == 2 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print "│\n".white
+        print "│ 3 │"
+        @@start_station_number == 3 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print "     28th        "
+        @@end_station_number == 3 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│ 3 │"
+        @@start_station_number == 3 && @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print "  Union Square      "
+        @@end_station_number == 3 && @@end_line_number == 2 ? print(set_color "red") : print(" ")
+        print "│ 3 │"
+        @@start_station_number == 3 && @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print"     28th         "
+        @@end_station_number == 3 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print"│\n".white
+        print "│ 4 │"
+        @@start_station_number == 4 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print "     23rd        "
+        @@end_station_number == 4 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│ 4 │"
+        @@start_station_number == 4 && @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print"       3rd          "
+        @@end_station_number == 4 && @@end_line_number == 2 ? print(set_color "red") : print(" ")
+        print "│ 4 │"
+        @@start_station_number == 4 && @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print "     23rd         "
+        @@end_station_number == 4 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print "│\n".white
+        print "│ 5 │"
+        @@start_station_number == 5 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print " Union Square    "
+        @@end_station_number == 5 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│ 5 │"
+        @@start_station_number == 5 && @@start_line_number == 2 ? print(set_color "green") : print(" ")
+        print"       1st          "
+        @@end_station_number == 5 && @@end_line_number == 2 ? print(set_color "red") : print(" ")
+        print "│ 5 │"
+        @@start_station_number == 5 && @@start_line_number == 3 ? print(set_color "green") : print(" ")
+        print " Union Square     "
+        @@end_station_number == 5 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print "│\n".white
+        print "│ 6 │"
+        @@start_station_number == 6 && @@start_line_number == 1 ? print(set_color "green") : print(" ")
+        print "     8th         "
+        @@end_station_number == 6 && @@end_line_number == 1 ? print(set_color "red") : print(" ")
+        print "│   │                      │ 6 │   Astor Place     "
+        @@end_station_number == 6 && @@end_line_number == 3 ? print(set_color "red") : print(" ")
+        print "│\n".white
+        puts  "└───┴───────────────────┴───┴──────────────────────┴───┴────────────────────┘".white
+
+        print " "
+        print "S".black.on_green
+        print " : Start Point ".white
+        print "E".white.on_red
+        print " : End point".white
+        puts "                                CTRL-C To EXIT\n\n"
+    end
+
+    def self.set_color color
+        if color == "green"
+            return "S".black.on_green
+        elsif color == "red"
+            return "E".light_white.on_red
+        end
+    end
+
+    def self.line_selection(journey)
+        if journey == "start"
+            print "Please enter your origin line number: "
+            @@start_line_number = gets.to_i
+            print "Please enter your origin station number: "
+            @@start_station_number = gets.to_i
+
+            # update menu
+            render_menu
+        elsif journey == "end"
+            print "Please enter your destination line number: "
+            @@end_line_number = gets.to_i
+            print "Please enter your destination station number: "
+            @@end_station_number = gets.to_i
+
+            # update menu
+            render_menu
+            Travel.plan_trip @@start_line_number, @@start_station_number, @@end_line_number, @@end_station_number
+            continue_trip
+        end
+    end
+
+    def self.continue_trip
+        print "\npress any key to continue"
+        STDIN.getch
+        system "clear"
+        @@start_line_number = 0
+        @@start_station_number = 0
+        @@end_line_number = 0
+        @@end_station_number = 0
+    end
 
 end
 
-line_N = Line.new ['Times Square', '34th', '28th', '23rd', 'Union Square', '8th'], 'Union Square'
-line_L = Line.new ['8th', '6th', 'Union Square', '3rd', '1st'], 'Union Square'
-line_6 = Line.new ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Astor Place'], 'Union Square'
-
+# binding.pry
 
 # `puts` shows output similar to this:
 # "You must travel through the following stops on the N line: 34th, 28th, 23rd, Union Square."
@@ -34,26 +249,17 @@ line_6 = Line.new ['Grand Central', '33rd', '28th', '23rd', 'Union Square', 'Ast
 # "Your journey continues through the following stops: 23rd, 28th, 33rd."
 # "7 stops in total."
 
-def plan_trip start_line, start_station, end_line, end_station
+# Travel.plan_trip line_N, 'Times Square', line_N, 'Union Square'
+# Travel.plan_trip line_N, 'Times Square', line_6, 'Grand Central'
 
-    # same line
-    start_index = start_line.stations.index(start_station)
-    end_index = end_line.stations.index(end_station)
-
-    if start_line == end_line
-        if start_index < end_index
-            p start_line.stations[start_index..end_index]
-        else
-            p start_line.stations[end_index..start_index].reverse
-        end
-    end
-    # line_N.stations[line_N.stations.index('Times Square')..line_N.stations.index('Union Square')]
+while true
+    Render.render_menu
+    start_line = Render.line_selection("start")
+    end_line = Render.line_selection("end")
 end
+# Travel.plan_trip 1, 2, 2, 3
+# Travel.plan_trip 1, 2, 3, 5
+# Travel.plan_trip 3, 5, 1, 2
+# Travel.plan_trip 3, 1, 1, 1
 
-plan_trip line_N, 'Times Square', line_N, 'Union Square'
-
-binding.pry
-
-# stop counter
-# traversal from point to point into array
-# check where lines intersect
+# binding.pry
