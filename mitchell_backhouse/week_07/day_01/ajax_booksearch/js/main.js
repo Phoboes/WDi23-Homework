0eec3fb1609d7c6096ae2291476cfb2b9d3f75f6
Mitchell Backhouse <mitchell.backhouse+github@gmail.com>
2017-09-04 17:36:38 +1000

    ajax_booksearch

---
+++ mitchell_backhouse/week_07/day_01/ajax_booksearch/js/main.js
@@ -0,0 +1,53 @@
+// https://www.googleapis.com/books/v1/volumes?q=title:Ulysses
+
+const bookSearch = function(search) {
+  const xhr = new XMLHttpRequest();
+
+  let val = document.getElementById('searchText').value;
+
+  xhr.open('GET', `https://www.googleapis.com/books/v1/volumes?q=title:${val}`);
+
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== 4) {
+      return;
+    }
+
+    const book = JSON.parse( xhr.responseText );
+    const title = $(`<li><strong>Title:</strong><p>${book.items[0].volumeInfo.title}</p></li>`);
+    const author = $(`<li><strong>Author:</strong><p>${book.items[0].volumeInfo.authors[0]}</p></li>`);
+    const subtitle = book.items[0].volumeInfo.subtitle;
+    const description = book.items[0].volumeInfo.description;
+    const bookThumb = book.items[0].volumeInfo.imageLinks.thumbnail;
+    const image = $(`<img src=${bookThumb}>`);
+
+    image.src = bookThumb;
+
+    let displayArea = $('.displayBookDetails');
+    displayArea.empty().append(image.clone());
+    displayArea.append('<ul></ul>');
+    $('.displayBookDetails ul').append(title);
+    $('.displayBookDetails ul').append(author);
+    //displayArea.append(title);
+    //displayArea.append(author);
+
+    displayArea.append("<br><strong>Description</strong><br>" + description.substring(0,300) + '...');
+
+    $('.bookList').append(image);
+    //document.body.appendChild(image);
+  }
+
+
+  xhr.send();
+  
+}
+
+$(document).ready( function () {
+  //$('#searchText').fadeToggle('slow', 'linear');
+
+  const button = document.getElementById('booksearch');
+  button.addEventListener('click', bookSearch);
+});
+
+
+
+
